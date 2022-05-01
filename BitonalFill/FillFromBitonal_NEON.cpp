95ec3dec058724c3bc254a4939c3281dd5fcf347
ptahmose <jbohl@h-quer.de>
2022-05-01 22:40:49 +0200

    neon

--- BitonalFill/FillFromBitonal_NEON.cpp
+++ BitonalFill/FillFromBitonal_NEON.cpp
@@ -34,7 +34,7 @@ void FillFromBitonalFromOnes_Gray8_NEON(
             uint8x8_t bitonal = vdup_n_u8(*ptrSrc);
             uint8x8_t vec = vtst_u8(bitSelectMask, bitonal);
             uint8x8_t notVec = vmvn_u8(vec);
-            uint8x8_t r = vorr_u8(vand_u8(vld1_u64((uint64_t*)ptrDst), notVec), vand_u8(vec, value));
+            uint8x8_t r = vorr_u8(vand_u8(vld1_u8(ptrDst), notVec), vand_u8(vec, value));
             vst1_u64(ptrDst, r);
 
             ++ptrSrc;
